9293d30070a4ccb54a366ab635b9c9c0cdc8ea01
Binyamin Lachkar <benjilachkar@gmail.com>
2025-04-19 22:26:48 +0300

    2.3

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -4,7 +4,7 @@ import { Product } from '@/app/lib/definitions'
 
 export default function ProductList({ products }: { products: Product[] }) {
   return (
-    <div className="">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
       {products.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
